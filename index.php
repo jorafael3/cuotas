d744cb07d720f8c6a7056d1fa18c3fbe6ff39dcf
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 16:14:34 -0500

    sd

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 
-require("conexion.php");
+// require("conexion.php");
 
-$nombre = 'l3250';
+// $nombre = 'l3250';
 // $sql = "WEB_Select_Productos_Cartimex_Like '".strtoupper($nombre)."', '' ";
 // $result = mssql_query(utf8_decode($sql));
 // // echo 'Registros encontrados: ' . mssql_num_rows($result) . '<br>';
@@ -11,9 +11,28 @@ $nombre = 'l3250';
 // print_r(mssql_fetch_array($result));
 
 
-$sql = "WEB_Select_Producto_2 '" . $nombre . "' ";
-$result = mssql_query(utf8_decode($sql));
+// $sql = "WEB_Select_Producto_2 '" . $nombre . "' ";
+// $result = mssql_query(utf8_decode($sql));
 // echo 'Registros encontrados: ' . mssql_num_rows($result) . '<br>';
-echo "<pre>";
-var_dump(mssql_fetch_array($result));
-echo "</pre>";
+// echo "<pre>";
+// var_dump(mssql_fetch_array($result));
+// echo "</pre>";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="200" src="186.3.23.4:82/SGO/">
+    </iframe>
+
+</body>
+
+</html>
