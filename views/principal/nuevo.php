331b7ba0f152b9553c60e8e87d95f25282e2e608
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 18:17:10 -0500

    asd

--- views/principal/nuevo.php
+++ views/principal/nuevo.php
@@ -15,35 +15,47 @@ require 'views/header.php';
         </div>
         <div class="card-body">
 
-            <div class="col-lg-12 row">
+            <table id="example" class="display nowrap" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                        <td>2011-04-25</td>
+                        <td>$320,800</td>
+                    </tr>
+                    <tr>
+                        <td>Garrett Winters</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>63</td>
+                        <td>2011-07-25</td>
+                        <td>$170,750</td>
+                    </tr>
 
-                <div class="col-lg-4 ">
-
-                    <label class="form-label fs-6 fw-bolder text-gray-700 mb-3">Cédula</label>
-                    <!--begin::Input group-->
-                    <div class="mb-5">
-                        <!-- <input type="text" class="form-control form-control-solid" placeholder="Cedula" /> -->
-                        <div class="input-group mb-0">
-
-                            <input id="PROF_CLI_CEDULA" type="text" class="form-control form-control-solid" placeholder="cedula" aria-label="cedula / nombre del cliente" aria-describedby="basic-addon2">
-
-                            <div class="input-group-append">
-                                <button onclick="Buscar_cedula()" class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
-
-
-
-            </div>
-            <div id="CON" style="display: none;">
-                <h1 style="font-size: 48px;" id="ENTRE" ></h1>
-                <div id="chartdiv" style="width: 100%;height: 800px;"></div>
-            </div>
-
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </div>
 </div>
@@ -58,5 +70,12 @@ require 'views/header.php';
 <?php require 'funciones/minimos_js.php'; ?>
 
 <script>
-
+    $(document).ready(function() {
+        $('#example').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+        });
+    });
 </script>
