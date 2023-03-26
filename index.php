e454720389cd4e5756b33ca9473e467b39c109b2
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 15:24:20 -0500

    sdf

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 require("conexion.php");
 
 $nombre = 'l3250';
-$sql = "WEB_Select_Productos_Cartimex_Like '".strtoupper($nombre)."', '' ";
+// $sql = "WEB_Select_Productos_Cartimex_Like '".strtoupper($nombre)."', '' ";
+// $result = mssql_query(utf8_decode($sql));
+// // echo 'Registros encontrados: ' . mssql_num_rows($result) . '<br>';
+// $count=mssql_num_rows($result);
+// print_r(mssql_fetch_array($result));
+
+
+$sql = "WEB_Select_Producto '" . $nombre . "' ";
 $result = mssql_query(utf8_decode($sql));
 // echo 'Registros encontrados: ' . mssql_num_rows($result) . '<br>';
-$count=mssql_num_rows($result);
 print_r(mssql_fetch_array($result));
