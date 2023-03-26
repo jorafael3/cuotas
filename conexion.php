d29d97a18963216f26eec89ab015d6dc4fbbdbea
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 14:44:27 -0500

    asd

--- conexion.php
+++ conexion.php
@@ -1,16 +1,17 @@
 <?php
+phpinfo();
 
-$DB_HOST = "10.5.1.3";
-$DB_USER = "jairo";
-$DB_PASS = "qwertys3gur0";
-$DB_NAME = "CARTIMEX";
+// $DB_HOST = "10.5.1.3";
+// $DB_USER = "jairo";
+// $DB_PASS = "qwertys3gur0";
+// $DB_NAME = "CARTIMEX";
 
-$min_link2pay = 800;
+// $min_link2pay = 800;
 
-/// Para MYSQL
-$con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die(mysqli_error());
-if (mysqli_connect_errno()) {
-    echo "No se ha podido conectar a MySQL: " . mysqli_connect_error();
-}
+// /// Para MYSQL
+// $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die(mysqli_error());
+// if (mysqli_connect_errno()) {
+//     echo "No se ha podido conectar a MySQL: " . mysqli_connect_error();
+// }
 
 ?>
