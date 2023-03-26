0dac48f771e902f7e14c568b6f8703761466c4b3
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 15:12:47 -0500

    xcv

--- conexion.php
+++ conexion.php
@@ -1,18 +1,34 @@
 <?php
 
-$DB_HOST = "10.5.1.3";
-$DB_USER = "mike";
-$DB_PASS = "Princes@";
-$DB_NAME = "CARTIMEX";
+$link = mssql_connect('10.5.1.3:1433', 'jairo', 'qwertys3gur0');
 
-$min_link2pay = 800;
+if (!$link)
+    die('Imposible conectar con el DOBRA!');
+else
+   //     echo mssql_get_last_message();
+    //    echo '<br>';
 
-/// Para MYSQL
-$con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die(mysqli_error());
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
+if (!mssql_select_db('CARTIMEX', $link))
+    die('No se puede seleccionar la base!');
+else
+ //        echo mssql_get_last_message();
+ //       echo '<br>';
+
+echo '<br>';
+
+// $DB_HOST = "10.5.1.3";
+// $DB_USER = "mike";
+// $DB_PASS = "Princes@";
+// $DB_NAME = "CARTIMEX";
+
+// $min_link2pay = 800;
+
+// /// Para MYSQL
+// $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die(mysqli_error());
+// if (mysqli_connect_errno()) {
+//     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//     exit();
+// }
 
 // $sql_serverName = "tcp:10.5.1.3,1433";
 // $sql_database = "CARTIMEX";
