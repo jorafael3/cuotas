ad53bddf26af70ac7c891fac671e0c919dc3e5d3
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 15:22:24 -0500

    awd

--- index.php
+++ index.php
@@ -3,14 +3,9 @@
 
 require("conexion.php");
 
-$sql = "SELECT * FROM inv_productos where Código = 'l3250'";
-//echo "Sql:".$sql."<br>";
-$result = mysqli_query($con, $sql);
-if(mysqli_fetch_array($result)){
-    echo "<pre>";
-    print_r($result);
-    echo "</pre>";
-}else{
-
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+$nombre = 'l3250';
+$sql = "WEB_Select_Productos_Cartimex_Like '".strtoupper($nombre)."', '' ";
+$result = mssql_query(utf8_decode($sql));
+// echo 'Registros encontrados: ' . mssql_num_rows($result) . '<br>';
+$count=mssql_num_rows($result);
+print_r(mssql_fetch_array($result));
