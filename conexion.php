7ce99ed110e30fd065d24b2944f2987b6fbb674b
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 15:17:32 -0500

    sdfsdf

--- conexion.php
+++ conexion.php
@@ -2,17 +2,21 @@
 
 $link = mssql_connect('10.5.1.3:1433', 'jairo', 'qwertys3gur0');
 
-if (!$link)
-    die('Imposible conectar con el DOBRA!');
-else
-   //     echo mssql_get_last_message();
-    //    echo '<br>';
+if (!$link) {
+    echo ('Imposible conectar con el DOBRA!');
+} else {
+    echo ('onectar con el DOBRA!');
+}
+//     echo mssql_get_last_message();
+//    echo '<br>';
 
-if (!mssql_select_db('CARTIMEX', $link))
-    die('No se puede seleccionar la base!');
-else
- //        echo mssql_get_last_message();
- //       echo '<br>';
+if (!mssql_select_db('CARTIMEX', $link)) {
+    echo ('No se puede seleccionar la base!');
+} else {
+    echo ('BASE CPNECTADA');
+}
+//        echo mssql_get_last_message();
+//       echo '<br>';
 
 echo '<br>';
 
@@ -40,4 +44,3 @@ echo '<br>';
 // } catch (PDOException $e){
 //   die('Connected failed:'.$e-> getMessage());
 // }
-
