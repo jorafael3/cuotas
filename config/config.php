1abaf01d79cb705975cdcf069273044f00f531c7
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 17:44:53 -0500

    sdf

--- config/config.php
+++ config/config.php
@@ -1,13 +1,15 @@
 <?php
 $link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $u = "$_SERVER[HTTP_HOST]";
-define('URL','http://'.$u.'/SGO/Cartimex/proteccionmarcas/');// ip local:puerto
+define('URL','http://'.$u.'/cuotas/');// ip local:puerto
 
-define('HOST', '10.5.1.86');
+define('HOST', '10.5.1.3');
 define('DB', 'CARTIMEX');
-define('USER', 'jalvarado');
-define('PASSWORD', 'jorge123');
+define('USER', 'jairo');
+define('PASSWORD', 'qwertys3gur0');
 define('CHARSET', 'utf8mb4');
+
+
 // define('HOST', '10.5.1.86');
 // define('USER', 'jalvarado');
 // define('PASSWORD', 'jorge123');
--- views/header.php
+++ views/header.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../boardcartimex2.php');
+// require_once('../boardcartimex2.php');
 
 ?>
 <!DOCTYPE html>
