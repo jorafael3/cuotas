225d946a709a6dbfe4aaabad53ce8a74c4fe2fa7
jorafael3 <jalvaradoe3@gmail.com>
2023-03-26 15:04:51 -0500

    dsf

--- index.php
+++ index.php
@@ -6,8 +6,11 @@ require("conexion.php");
 $sql = "SELECT * FROM inv_productos where Código = 'l3250'";
 //echo "Sql:".$sql."<br>";
 $result = mysqli_query($con, $sql);
-$row = mysqli_fetch_array($result);
-echo "<pre>";
-print_r($row);
-echo "</pre>";
-?>
+if(mysqli_fetch_array($result)){
+    echo "<pre>";
+    print_r($result);
+    echo "</pre>";
+}else{
+
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
